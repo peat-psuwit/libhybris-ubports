c840210d5a83791a88b3e3ffaedd0f6df94dffd9
Ratchanan Srirattanamet <ratchanan@ubports.com>
2020-11-01 01:01:43 +0700

    glvnd: remove reference to Mesa's _eglError
    Unfortunately, libhybris doesn't have its own error system. So, we won't
    be able to store errors in these cases.

--- hybris/egl/glvnd/egldispatchstubs.c
+++ hybris/egl/glvnd/egldispatchstubs.c
@@ -4,8 +4,6 @@
 #include <string.h>
 #include <stdlib.h>
 
-#include "eglcurrent.h"
-
 static const __EGLapiExports *exports;
 
 const int __EGL_DISPATCH_FUNC_COUNT = __EGL_DISPATCH_COUNT;
@@ -59,12 +57,9 @@ static __eglMustCastToProperFunctionPointerType FetchVendorFunc(__EGLvendorInfo 
     }
     if (func == NULL) {
         if (errorCode != EGL_SUCCESS) {
-            // Since we have no vendor, the follow-up eglGetError() call will
-            // end up using the GLVND error code. Set it here.
-            if (vendor == NULL) {
-                exports->setEGLError(errorCode);
-            }
-            _eglError(errorCode, __EGL_DISPATCH_FUNC_NAMES[index]);
+            // Libhybris doesn't have its own EGL error system. Set the error
+            // to GLVND instead.
+            exports->setEGLError(errorCode);
         }
         return NULL;
     }
@@ -72,7 +67,6 @@ static __eglMustCastToProperFunctionPointerType FetchVendorFunc(__EGLvendorInfo 
     if (!exports->setLastVendor(vendor)) {
         // Don't bother trying to set an error code in libglvnd. If
         // setLastVendor failed, then setEGLError would also fail.
-        _eglError(errorCode, __EGL_DISPATCH_FUNC_NAMES[index]);
         return NULL;
     }
 
